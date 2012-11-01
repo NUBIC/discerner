85146e6820ce1d343eadb45f3c5f5b7bb9a08d55
Yulia Bushmanova <y.bushmanova@gmail.com>
2012-11-01 15:59:28 -0500

    Fix HABTM joining table name for Operator and ParameterType models

--- app/models/discerner/parameter_type.rb
+++ app/models/discerner/parameter_type.rb
@@ -1,7 +1,7 @@
 module Discerner
   class ParameterType < ActiveRecord::Base
     has_many        :parameters
-    has_and_belongs_to_many :operators
+    has_and_belongs_to_many :operators, :join_table => :discerner_operators_parameter_types
     
     validates       :name, :presence => true, :uniqueness => {:scope => :deleted_at, :message => "for parameter type has already been taken"}
     attr_accessible :deleted_at, :name
