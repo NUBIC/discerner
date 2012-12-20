e06e9b214bd904c5ba7d307bffa58646bf992cc1
Michael Gurley <m-gurley@northwestern.edu>
2012-12-20 12:00:37 -0600

    Adding an empty value programmatically to the combo box added via JavaScript, so that if a user submits a selected criteria without a chosen value then the first values is not automatically used.

--- app/assets/javascripts/discerner/search_parameter.js
+++ app/assets/javascripts/discerner/search_parameter.js
@@ -27,11 +27,14 @@ Discerner.SearchParameter.UI = function (config) {
       } else if ($(selectedParameter).hasClass('combobox')) {
         var input = $(row).find('input.parameter_value_id');
         if (input.length > 0) {
- 
+
           var select = $('<select>').attr('id', input.attr('id'))
             .attr('name', input.attr('name'))
             .addClass('parameter_values_combobox_autocompleter')
             .insertBefore(input);
+
+          var optionNone = $('<option>').val('').html('');
+          optionNone.appendTo(select);
 
           $.get(parametersUrl.sub({ question_id: selectedParameter.val() }), function (data) {
             $.each(data.parameter_values, function() {
@@ -42,18 +45,18 @@ Discerner.SearchParameter.UI = function (config) {
               option.appendTo(select);
             });
           });
-          input.detach();  
+          input.detach();
         }
         $(config.container).find('.parameter_value, .remove').show();
-        $(config.container).find('a.add_search_parameter_values').show();     
+        $(config.container).find('a.add_search_parameter_values').show();
       }
       $(config.container).find('.nested_records_search_parameter_values tr.search_parameter_value:visible:not(:first)')
         .find('.parameter_values_boolean_operator span').html('OR');
       $(".parameter_values_combobox_autocompleter").combobox({watermark:'a value', css_class:'autocompleter-dropdown'});
-      
+
       var i = 0,
           display_orders = config.container.find('input[name*="search_parameter_values_attributes"][name$="[display_order]"]');
-          
+
       // get max display order
       $.each(display_orders, function(){
         var val = parseInt($(this).val());
@@ -66,11 +69,11 @@ Discerner.SearchParameter.UI = function (config) {
           $(this).val(i);
         }
       });
-      
+
     },
-    searchParameterValuesNestedAttributesForm = new NestedAttributes({ 
-      container: config.container, 
-      association: 'search_parameter_values', 
+    searchParameterValuesNestedAttributesForm = new NestedAttributes({
+      container: config.container,
+      association: 'search_parameter_values',
       content: config.searchParameterValuesTemplate,
       addHandler: setupParameterValues,
       caller: this
@@ -78,7 +81,7 @@ Discerner.SearchParameter.UI = function (config) {
 
   setupParameterValues();
   $.each($('.operator'), function() { setupOperator(this); });
-  
+
   $('.operator select').live('change', function(){
     setupOperator(this);
   });
