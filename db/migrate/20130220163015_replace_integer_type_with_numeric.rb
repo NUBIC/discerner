80be8798f331e47f71770823fb4bc58be21b8d3b
Yulia Bushmanova <y.bushmanova@gmail.com>
2013-02-20 11:28:28 -0600

    fixed migration

--- db/migrate/20130220163015_replace_integer_type_with_numeric.rb
+++ db/migrate/20130220163015_replace_integer_type_with_numeric.rb
@@ -1,3 +1,4 @@
+# This migration comes from discerner (originally 20130220163015)
 class ReplaceIntegerTypeWithNumeric < ActiveRecord::Migration
   class ParameterType < ActiveRecord::Base
     include Discerner::Methods::Models::ParameterCategory
@@ -5,13 +6,17 @@ class ReplaceIntegerTypeWithNumeric < ActiveRecord::Migration
   
   def up
     parameter_type = Discerner::ParameterType.find_by_name('integer')
-    parameter_type.name = 'numeric'
-    parameter_type.save!
+    unless parameter_type.blank?
+      parameter_type.name = 'numeric'
+      parameter_type.save!
+    end
   end
 
   def down
     parameter_type = Discerner::ParameterType.find_by_name('numeric')
-    parameter_type.name = 'integer'
-    parameter_type.save!
+    unless parameter_type.blank?
+      parameter_type.name = 'integer'
+      parameter_type.save!
+    end
   end
 end
