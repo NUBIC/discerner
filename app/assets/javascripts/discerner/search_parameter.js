05ddcd693abfa778d2ae2265b63b2599db95ecd4
Yulia Bushmanova <y.bushmanova@gmail.com>
2013-03-19 00:52:17 -0500

    Fixed disabling parameter selections in IE for removed parameters

--- app/assets/javascripts/discerner/search_parameter.js
+++ app/assets/javascripts/discerner/search_parameter.js
@@ -5,7 +5,7 @@ Discerner.SearchParameter.UI = function (config) {
   var parametersUrl = new Discerner.Url(config.parametersUrl),
   dictionarySelector = $('.discerner_search_dictionary select#search_dictionary_id'),
   selectedDictionaryOption = $(dictionarySelector).find('option:selected:last'),
-      
+
   setupParameters = function () {
     var search_parameters = $('.search_parameters').find('.nested_records_search_parameters tr.search_parameter'),
     display_orders = search_parameters.find('input:not([name*="search_parameter_values_attributes"])[name$="[display_order]"]'),
@@ -13,13 +13,13 @@ Discerner.SearchParameter.UI = function (config) {
 
     search_parameters.filter(':visible:not(:first)').find('.parameter_boolean_operator span').html('and');
     $(".parameters_combobox_autocompleter").combobox({watermark:'a question'});
-        
+
     // get max display order
     $.each(display_orders, function(){
       var val = parseInt($(this).val());
       if (val >= i) { i = val }
     });
-        
+
     // assign display order to search predicates without it
     $.each(display_orders, function(){
       if ($(this).val().length == 0) {
@@ -27,7 +27,7 @@ Discerner.SearchParameter.UI = function (config) {
         $(this).val(i);
       }
     })
-        
+
     // hide parameter options that do not belong to selected dictionary
     if (dictionarySelector.length > 0){
       $.each($('div.parameter_category'), function(){
@@ -50,20 +50,19 @@ Discerner.SearchParameter.UI = function (config) {
 
   // handle criteria autocompleter button click
   $(document).on('click', '.categorized_autocompleter_link', function () {
-    var select = $(this).siblings('select').first(),    
+    var select = $(this).siblings('select').first(),
         sibling_selects = $('select.' + select.attr('class')).filter(function(){
-          return $(this).closest('tr').find('td.remove input[name$="[_destroy]"]:not([name*="[search_parameter_values_attributes]"])[value="1"]').length == 0 // exclude rows marked for destroy
+          return $(this).closest('tr').find('td.remove input[name$="[_destroy]"]:not([name*="[search_parameter_values_attributes]"])').filter(function() { return this.value === '1'; }).length == 0 // exclude rows marked for destroy
         }),  // get all the "sibling" dropdowns
         matching_selected_options = sibling_selects.find('option.exclusive:selected'),
         popup = $(this).siblings('.div-criteria-popup'); // find selected options in "sibling" dropdowns that match current option value (do not count if option text matches "Text search diagnosis")
-
     popup.find('.criteria a.categorized_autocompleter_item_link').removeClass('selection_disabled');
     $.each(matching_selected_options, function(){
       popup.find('.criteria a.categorized_autocompleter_item_link[rel="' + $(this).html() + '"]').addClass('selection_disabled');
-    })    
+    })
     toggleCategorizedAutocompleterPopup(this);
   });
-  
+
   // handle criteria popup list link click
   $(document).on('click', '.categorized_autocompleter_item_link:not(.selection_disabled)', function () {
     var autocompleter = $(this).parents('.categorized_autocompleter').find('.parameters_combobox_autocompleter'),
@@ -95,14 +94,14 @@ Discerner.SearchParameter.UI = function (config) {
       predicate = $(that).closest('.search_parameter'),
       selected_option = $(that).find('option:selected:last'),
       add_button = $(predicate).find('a.add_search_parameter_values');
-      
+
     $(predicate).find('.nested_records_search_parameter_values .search_parameter_value .delete_search_parameter_values').click();
     if ($(selected_option).length == 0 || $(selected_option).val() == ''){
       $(add_button).hide();
     }
     else if ($(selected_option).hasClass('list')){
       $($(predicate).find('.tmp_link')).remove();
-      
+
       $.get(parametersUrl.sub({ question_id: this.value }), function (data) {
        $.each(data.parameter_values, function() {
          $(add_button).click();
