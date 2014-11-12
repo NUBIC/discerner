77d41546aede39fa4e09d23dff1447c6bc58a9cd
Yulia Bushmanova <y-bushmanova@northwestern.edu>
2014-11-12 12:37:17 -0600

    Move jQueryUI assets under discerner assets.

--- app/assets/javascripts/discerner/application.js
+++ app/assets/javascripts/discerner/application.js
@@ -12,6 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
-//= require jquery.blockUI
+//= require discerner/jquery/jquery-ui
+//= require discerner/jquery/jquery.blockUI
 //= require_tree .
